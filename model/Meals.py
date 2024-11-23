114677b384ae687677e7b1d7eb9ed44d59a84b38
lucasnather <natherlucas@gmail.com>
2024-11-22 22:35:12 -0400

    feat: create model meals

--- model/Meals.py
+++ model/Meals.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class Meals(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column()
-    description: Mapped[str] = mapped_column()
-    datetime: Mapped[datetime] =  mapped_column(default=datetime.now())
-    isOnDiet: Mapped[bool] = mapped_column()
+    name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    datetime: Mapped[datetime] =  mapped_column(default=datetime.now(), nullable=False)
+    isOnDiet: Mapped[bool] = mapped_column(nullable=False)
