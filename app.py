00ba5f30352ee5d63f322a9332d23b4234952d72
lucasnather <natherlucas@gmail.com>
2024-11-22 23:43:09 -0400

    feat: register a meal

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
-from flask import Flask, request
-from config.database import db
+from flask import Flask, request, jsonify
+from database import db
+from model.Meals import Meals
 
 app = Flask(__name__)
 
-app.config["SQLACHEMY_DATABASE_URI"] = "mysql://nather:podevermesmosotoestudando@localhost:3307/diet"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db.init_app(app)
 
 @app.route("/")
 def hello_world():
     return "Hello World"
+
+@app.route("/api/meals", methods=["POST"])
+def register_meal():
+    data = request.json
+    name = data.get("name")
+    description = data.get("description")
+    isOnDiet = data.get("isOnDiet")
+
+    meal = Meals(name=name, description=description, isOnDiet=isOnDiet)
+    db.session.add(meal)
+    db.session.commit()
+
+    mealToDict = meal.to_dict()
+
+    return jsonify({
+        "meals": mealToDict
+    }), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model/Meals.py
+++ model/Meals.py
@@ -1,10 +1,19 @@
-from config.database import db
-from sqlalchemy.orm import Mapped, mapped_column
-from datetime import datetime 
+from database import db
+from datetime import datetime
 
 class Meals(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    description: Mapped[str] = mapped_column(nullable=False)
-    datetime: Mapped[datetime] =  mapped_column(default=datetime.now(), nullable=False)
-    isOnDiet: Mapped[bool] = mapped_column(nullable=False)
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    isOnDiet = db.Column(db.Boolean, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "datetime": self.datetime,
+            "isOnDiet": self.isOnDiet,
+        }
