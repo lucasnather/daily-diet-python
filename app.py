acab9067d5b129d3ddb688b353e87bce026fc895
lucasnather <natherlucas@gmail.com>
2024-11-22 22:23:34 -0400

    feat: config database

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
+from config.database import db
 
 app = Flask(__name__)
+
+app.config["SQLACHEMY_DATABASE_URI"] = "mysql://nather:podevermesmosotoestudando@localhost:3307/diet"
+
+db.init_app(app)
 
 @app.route("/")
 def hello_world():
