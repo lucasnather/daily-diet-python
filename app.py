a6f44d5929fa756d3c93ab5ac2c9dd6348d5a598
lucasnather <natherlucas@gmail.com>
2024-11-25 20:45:10 -0400

    feat: add methods GET, DELETE , PUT and PATCH

--- app.py
+++ app.py
@@ -29,5 +29,101 @@ def register_meal():
         "meals": mealToDict
     }), 201
 
+@app.route("/api/meals", methods=["GET"])
+def get_meal():
+    meals = Meals.query.all()
+
+    meals_dict = [{ "id": meal.id, "name": meal.name, "description": meal.description, "isOnDiet": meal.isOnDiet } for meal in meals]
+
+    return jsonify({
+        "meals": meals_dict
+    }), 200
+
+@app.route("/api/meals/<int:meal_id>", methods=["GET"])
+def get_meal_by_id(meal_id):
+    meal = Meals.query.filter_by(id=meal_id).first()
+
+    if not meal:
+        return jsonify({"error": "Meal Not Found"})
+    
+    meal_to_dict = {
+        "id": meal.id,
+        "name": meal.name,
+        "description": meal.description,
+        "isOnDiet": meal.isOnDiet,
+    }
+
+    return jsonify({
+        "meals": meal_to_dict
+    }), 200
+
+@app.route("/api/meals/<int:meal_id>", methods=["PUT"])
+def update_meal(meal_id):
+    meal = Meals.query.filter_by(id=meal_id).first()
+
+    if not meal:
+        return jsonify({"error": "Meal Not Found"})
+    
+    data = request.json
+    
+    name = data.get("name")
+    description = data.get("description")
+    isOnDiet = data.get("isOnDiet")
+    
+    meal.name = name
+    meal.description = description
+    meal.isOnDiet = isOnDiet
+
+    db.session.commit()
+    
+    meal_to_dict = {
+        "id": meal.id,
+        "name": meal.name,
+        "description": meal.description,
+        "isOnDiet": meal.isOnDiet,
+    }
+
+    return jsonify({
+        "meals": meal_to_dict
+    }), 200
+
+@app.route("/api/meals/on-diet/<int:meal_id>", methods=["PATCH"])
+def update_meal_to_on_diet(meal_id):
+    meal = Meals.query.filter_by(id=meal_id).first()
+
+    if not meal:
+        return jsonify({"error": "Meal Not Found"})    
+
+    meal.isOnDiet = True
+
+    db.session.commit()
+    
+    meal_to_dict = {
+        "id": meal.id,
+        "name": meal.name,
+        "description": meal.description,
+        "isOnDiet": meal.isOnDiet,
+    }
+
+    return jsonify({
+        "meals": meal_to_dict
+    }), 200
+
+
+@app.route("/api/meals/<int:meal_id>", methods=["DELETE"])
+def delete_meal(meal_id):
+    meal = Meals.query.filter_by(id=meal_id).first()
+
+    if not meal:
+        return jsonify({"error": "Meal Not Found"})
+    
+    meal_id = meal.id
+    db.session.delete(meal)
+    db.session.commit()
+
+    return jsonify({
+        "message": f"Meal {meal_id} delete"
+    })
+
 if __name__ == "__main__":
     app.run(debug=True)
